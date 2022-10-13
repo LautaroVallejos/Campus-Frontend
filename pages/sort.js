29b75b869fa99a6ca1be0f82405229dc76c9ff24
Lautaro Vallejos <lautaro.vallejosk4m@gmail.com>
2022-10-13 17:44:23 -0300

    minor changes

--- pages/sort.js
+++ pages/sort.js
@@ -1,7 +1,7 @@
 import { fetcher } from "../lib/api";
-import { Table, useAsyncList } from "@nextui-org/react";
+import { Table, useAsyncList, useCollator } from "@nextui-org/react";
 
-const Tabla = () => {
+const Tabla = ( {estudiantes} ) => {
     const columns = [
         { name: "Nombre", uid: "nombre" },
         { name: "Apellido", uid: "apellido" },
@@ -13,15 +13,9 @@ const Tabla = () => {
         { name: "DNI", uid: "DNI" },
       ];
 
-      async function load({signal, cursor}) {
-        const res = fetcher('http://localhost:1337/api/estudiantes')
-        return {
-            items: res.results,
-            cursor: res.next
-        };
-    }
-
-    const estudiantes = useAsyncList(load);
+    
+    console.log({estudiantes})
+    const listaEstudiantes = useAsyncList(load);
         
         return (
             <Table
@@ -37,12 +31,77 @@ const Tabla = () => {
                     )}
                 </Table.Header>
 
-                <Table.Body>
+                <Table.Body
+                    items={listaEstudiantes.items}
+                    loadingState={listaEstudiantes.loadingState}
+                    onLoadMore={listaEstudiantes.loadMore}
+                    estudiantes={estudiantes}
+                >
+
+                    <Table.Row>
+                        <Table.Cell>
+                            Testing
+                        </Table.Cell>
+                        <Table.Cell>
+                            Testing
+                        </Table.Cell>
+                        <Table.Cell>
+                            Testing
+                        </Table.Cell>
+                        <Table.Cell>
+                            Testing
+                        </Table.Cell>
+                        <Table.Cell>
+                            Testing
+                        </Table.Cell>
+                        <Table.Cell>
+                            Testing
+                        </Table.Cell>
+                        <Table.Cell>
+                            Testing
+                        </Table.Cell>
+                        <Table.Cell>
+                            Testing
+                        </Table.Cell>
+                    </Table.Row>
+
+                    {estudiantes &&
+                        estudiantes.data.map((estudiante) => {
+                    return(
+                        <li key={estudiante.id}>
+                            <a href={`estudiante/` + estudiante.id}>
+                                <h2>Estudiante</h2>
+                                <div>
+                                    <h3>Nombre: {estudiante.attributes.nombre}</h3>
+                                    <h3>Apellido: {estudiante.attributes.apellido}</h3>
+                                    <h3>Edad: {estudiante.attributes.edad}</h3> 
+                                    <h3>Email: {estudiante.attributes.email}</h3> 
+                                    <h3>Contacto del Tutor: {estudiante.attributes.contactoTutor}</h3> 
+                                    <h3>Obra Social: {estudiante.attributes.obraSocial}</h3>
+                                    <h3>Nro Obra Social: {estudiante.attributes.numObraSocial}</h3> 
+                                    <h3>DNI: {estudiante.attributes.dni}</h3>  
+                                </div>
+                            </a>
+                        </li>
+                    );
+                })}
 
                 </Table.Body>
             </Table>
         )
 
+}
+
+async function load({signal, cursor}) {
+    const res = fetch('http://localhost:1337/api/estudiantes');
+    const estudiantes = await res.json();
+    return {
+        props: {
+            estudiantes: estudiantes.data
+        },
+        items: estudiantes.results,
+        cursor: estudiantes.next,
+    };
 }
 
 export default Tabla;
