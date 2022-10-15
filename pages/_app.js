39dcc2a74b12065e220a1110fd669e9f433b909a
avila nicolas <aviladanielnicolas@gmail.com>
2022-10-15 00:42:53 -0300

    WIP index desde 0

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import '../styles/navbar.css'
 import { NextUIProvider } from '@nextui-org/react';
 import Navbar from '../components/Navbar'
+//import 'bootstrap/dist/css/bootstrap.css'
 
 function MyApp({ Component, pageProps }) {
   return (
