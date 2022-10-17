2bcc8cc7c16fc458194a13efe072baca88a3e55c
avila nicolas <aviladanielnicolas@gmail.com>
2022-10-16 21:53:58 -0300

    continue navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,88 @@
+import { Divider } from "@nextui-org/react";
+import Image from "next/image";
 
    const Navbar = () => {
       return (
+          // {/* contenedor del navbar */}
+        <div className="cont-navbar">
+          <div className="title">
+            <Image
+              className="logoJh"
+              src="/img/jh-logo.png"
+              alt="logo"
+              width={35}
+              height={35}
+            />
+            <h4 className="title-name">Campus José Hernández</h4>
+        </div>
+        {/* separador */}
+        <hr className="linea"></hr>
         
-        <h1>hola</h1>
-    
-  
-  ) };
+        {/* contenedor paginas */}
+        {/* link de iconos https://iconos8.es/icons/set/estudiantes */}
+        <ul>
+          <div>
+
+            <div >
+              <a href="">
+                <li className="optionAlum">
+                <Image
+                  className="ico"
+                  src={"/img/alumIcon.png"}
+                  width={40}
+                  height={40}
+                  />
+                  <h4 className="option">Datos del estudiante</h4>
+                </li>
+              </a>
+              </div>
+
+          
+              <div >
+              <a href="">
+                <li className="optionAlum">
+                <Image
+                  className="ico"
+                  src={"/img/tablaIco.png"}
+                  width={40}
+                  height={40}
+                  />
+                  <h4 className="option">Tabla</h4>
+                </li>
+              </a>
+              </div>
+
+              <div >
+              <a href="">
+                <li className="optionAlum">
+                <Image
+                  className="ico"
+                  src={"/img/preceIcon.png"}
+                  width={40}
+                  height={40}
+                  />
+                  <h4 className="option">Preceptores</h4>
+                </li>
+              </a>
+              </div>
+
+              <div >
+              <a href="">
+                <li className="optionAlum">
+                <Image
+                  className="ico"
+                  src={"/img/inicioIcon.png"}
+                  width={40}
+                  height={40}
+                  />
+                  <h4 className="option">inicio de sesion</h4>
+                </li>
+              </a>
+              </div>
+
+          </div>
+        </ul>
+        </div>
+  )};
 
 export default Navbar;
