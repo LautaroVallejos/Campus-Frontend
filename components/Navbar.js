e64b7a7dbcb552ac738907fd3e5ec823843b5cc7
avila nicolas <aviladanielnicolas@gmail.com>
2022-10-22 00:50:54 -0300

    avance navbar responsive

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,22 @@
 import { Divider } from "@nextui-org/react";
 import Image from "next/image";
 
+// var check = document.getElementById("Menu")
+function MenuF() {
+  if (document.getElementById("Menu").checked) {
+    const menu=document.getElementById("cont-slider").style.display="block";
+  }
+  else{
+    const menu1=document.getElementById("cont-slider").style.display="hidden";
+  }
+}
+
+
    const Navbar = () => {
       return (
           // {/* contenedor del navbar */}
-          <div>
-        <div className="cont-navbar">
+      <div>
+        <div id="cont-slider" className="cont-navbar">
           <div className="title">
             <Image
               className="logoJh"
@@ -15,77 +26,91 @@ import Image from "next/image";
               height={35}
             />
             <h4 className="title-name">Campus José Hernández</h4>
-        </div>
-        {/* separador */}
-        <hr className="linea"></hr>
+          </div>
+          {/* separador */}
+          <hr className="linea"></hr>
         
         {/* contenedor paginas */}
         {/* link de iconos https://iconos8.es/icons/set/estudiantes */}
-        <ul>
-          <div>
+          <ul>
+            <div>
 
-            <div >
-              <a href="">
-                <li className="optionAlum">
-                <Image
-                  className="ico"
-                  src={"/img/alumIcon.png"}
-                  width={40}
-                  height={40}
-                  />
-                  <h4 className="option">Datos del estudiante</h4>
-                </li>
-              </a>
-              </div>
+              <div >
+                <a href="">
+                  <li className="optionAlum">
+                  <Image
+                    className="ico"
+                    src={"/img/alumIcon.png"}
+                    width={40}
+                    height={40}
+                    />
+                    <h4 className="option">Datos del estudiante</h4>
+                  </li>
+                </a>
+                </div>
 
+            
+                <div >
+                <a href="">
+                  <li className="optionAlum">
+                  <Image
+                    className="ico"
+                    src={"/img/tablaIco.png"}
+                    width={40}
+                    height={40}
+                    />
+                    <h4 className="option">Tabla</h4>
+                  </li>
+                </a>
+                </div>
+
+                <div >
+                <a href="">
+                  <li className="optionAlum">
+                  <Image
+                    className="ico"
+                    src={"/img/preceIcon.png"}
+                    width={40}
+                    height={40}
+                    />
+                    <h4 className="option">Preceptores</h4>
+                  </li>
+                </a>
+                </div>
+
+                <div >
+                <a href="">
+                  <li className="optionAlum">
+                  <Image
+                    className="ico"
+                    src={"/img/inicioIcon.png"}
+                    width={40}
+                    height={40}
+                    />
+                    <h4 className="option">inicio de sesion</h4>
+                  </li>
+                </a>
+                </div>
+
+            </div>
+          </ul>
+        </div>
+
+        <div className="menu-responsive">
+          <input type={"checkbox"} id="Menu" Click={MenuF} ></input>
+          <label for="Menu">
+          <Image
+          className="Icon-menu"
+          src={"/img/menuIcon.png"}
+          width={60}
+          height={60}
           
-              <div >
-              <a href="">
-                <li className="optionAlum">
-                <Image
-                  className="ico"
-                  src={"/img/tablaIco.png"}
-                  width={40}
-                  height={40}
-                  />
-                  <h4 className="option">Tabla</h4>
-                </li>
-              </a>
-              </div>
-
-              <div >
-              <a href="">
-                <li className="optionAlum">
-                <Image
-                  className="ico"
-                  src={"/img/preceIcon.png"}
-                  width={40}
-                  height={40}
-                  />
-                  <h4 className="option">Preceptores</h4>
-                </li>
-              </a>
-              </div>
-
-              <div >
-              <a href="">
-                <li className="optionAlum">
-                <Image
-                  className="ico"
-                  src={"/img/inicioIcon.png"}
-                  width={40}
-                  height={40}
-                  />
-                  <h4 className="option">inicio de sesion</h4>
-                </li>
-              </a>
-              </div>
-
-          </div>
-        </ul>
+          />
+          </label>
         </div>
+
       
-        </div>
+      </div>
   )};
 
 export default Navbar;
