8fe2df169cb0e5f834b2445444216e52b3ffea61
avila nicolas <aviladanielnicolas@gmail.com>
2022-10-22 20:37:42 -0300

    navbar responsive/ cambiar iconos

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,6 @@
 import { Divider } from "@nextui-org/react";
 import Image from "next/image";
 
-function MenuF() {
-  var check = document.getElementById("Menu").checked
-  if (check) {
-    const menu=document.getElementById("cont-slider").style.display="block";
-  }
-  else{
-    const menu=document.getElementById("cont-slider").style.display="none";
-  }
-}
 
 
    const Navbar = () => {
@@ -32,6 +23,98 @@ function MenuF() {
         
         {/* contenedor paginas */}
         {/* link de iconos https://iconos8.es/icons/set/estudiantes */}
+          <ul>
+            <div>
+
+              <div >
+                <a href="">
+                  <li className="optionAlum">
+                  <Image
+                    className="ico"
+                    src={"/img/alumIcon.png"}
+                    width={40}
+                    height={40}
+                    />
+                    <h4 className="option">Datos del estudiante</h4>
+                  </li>
+                </a>
+                </div>
+
+            
+                <div >
+                <a href="">
+                  <li className="optionAlum">
+                  <Image
+                    className="ico"
+                    src={"/img/tablaIco.png"}
+                    width={40}
+                    height={40}
+                    />
+                    
+                    <h4 className="option">Tabla</h4>
+                  </li>
+                </a>
+                </div>
+
+                <div >
+                <a href="">
+                  <li className="optionAlum">
+                  <Image
+                    className="ico"
+                    src={"/img/preceIcon.png"}
+                    width={40}
+                    height={40}
+                    />
+                    <h4 className="option">Preceptores</h4>
+                  </li>
+                </a>
+                </div>
+
+                <div >
+                <a href="">
+                  <li className="optionAlum">
+                  <Image
+                    className="ico"
+                    src={"/img/inicioIcon.png"}
+                    width={40}
+                    height={40}
+                    />
+                    <h4 className="option">inicio de sesion</h4>
+                  </li>
+                </a>
+                </div>
+
+            </div>
+          </ul>
+        </div>
+
+
+        {/* Menu responsive */}
+
+        <div className="menu-responsive">
+          <div className="btn-Menu">
+            <label className="menu" for="btn-menu">
+              <Image
+              src="/img/menuIcon.png"
+              width={40}
+              height={40}
+              />Menu</label>
+          </div>
+          <input type="checkbox" id="btn-menu"/>
+            
+            <div class="container-menu">
+                
+              <div class="cont-menu">
+                <div className="title">
+                  <Image
+                    className="logoJh"
+                    src="/img/jh-logo.png"
+                    alt="logo"
+                    width={35}
+                    height={35}
+                  />
+                  <h4 className="title-name">Campus José Hernández</h4>
+                </div>
           <ul>
             <div>
 
@@ -94,19 +177,9 @@ function MenuF() {
 
             </div>
           </ul>
-        </div>
-
-        <div className="menu-responsive">
-          <input type={"checkbox"} id="Menu" onClick={MenuF} ></input>
-          <label for="Menu">
-          <Image
-          className="Icon-menu"
-          src={"/img/menuIcon.png"}
-          width={60}
-          height={60}
-          
-          />
-          </label>
+                <label for="btn-menu">Cerrar ✖️</label>
+              </div>
+            </div>
         </div>
 
       
