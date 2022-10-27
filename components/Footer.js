e52fa88e8148f2ec16e4b35fc53b02085f719add
franncho <rarugfrancisco@gmail.com>
2022-10-26 21:25:56 -0300

    Avances de footer

--- components/Footer.js
+++ components/Footer.js
@@ -18,12 +18,12 @@ const Footer = () => {
                                 width={35}
                                 height={35}
                                 />
-                                    <h4>Ubicacion</h4>
+                                    <h4><a href='https://www.google.com/maps/dir/-34.6942996,-58.3374685/tecnica+7+jose+hernandez/@-34.6784563,-58.3662761,14z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x95a33351d983fa69:0x35e88ee685a84dd5!2m2!1d-58.3640341!2d-34.6618536'>Ubicacion</a></h4>
                                     <span><a href='https://www.google.com/maps/dir/-34.6942996,-58.3374685/tecnica+7+jose+hernandez/@-34.6784563,-58.3662761,14z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x95a33351d983fa69:0x35e88ee685a84dd5!2m2!1d-58.3640341!2d-34.6618536'>Ing. Marconi 745</a></span>
                                 </div>
                                 </div>
                             </div>
-                            <div className={styles.items}>
+                            <div className={styles.items2}>
                                 <div >
                                 <i />
                                 <Image
@@ -33,7 +33,7 @@ const Footer = () => {
                                 height={35}
                                 />
                                 <div>
-                                    <h4>Contacto Telefonico</h4>
+                                    <h4><a href="tel:+5442014180">Contacto Telefonico</a></h4>
                                     <span><a href="tel:+5442014180">4201-4180</a></span>
                                 </div>
                                 </div>
@@ -48,7 +48,7 @@ const Footer = () => {
                                 width={35}
                                 height={35}
                                 />
-                                    <h4>Email Oficial</h4>
+                                    <h4><a href='mailto:josehernandez@tecnica7.edu.ar'>Email Oficial</a></h4>
                                     <span><a href='mailto:josehernandez@tecnica7.edu.ar'>josehernandez@tecnica7.edu.ar</a></span>
                                 </div>
                                 </div>
@@ -60,10 +60,10 @@ const Footer = () => {
                     <hr className='hr'></hr>
                     <div >
                     <div  className={styles.copy}>
-                                    <p>Avila Nicolas, Cordon Alejo, Ramos Thiago, Rarug Francisco, Vallejos lautaro </p>
+                                    <p><strong>Avila Nicolas, Cordon Alejo, Ramos Thiago, Rarug Francisco, Vallejos Lautaro</strong></p>
                                 </div>
                     <div  className={styles.copy}>
-                                    <p>Copyright © 2022, All Right Reserved </p>
+                                    <p><em>Copyright © 2022, All Right Reserved</em></p>
                                 </div>            
                     
                     </div>
