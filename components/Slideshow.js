3c65db8bfa519b5a8441e67d49c10dd3b61a82fb
Franncho <rarugfrancisco@gmail.com>
2022-10-27 10:45:31 -0300

    carreadita del vene

--- components/Slideshow.js
+++ components/Slideshow.js
@@ -2,7 +2,7 @@ import React from "react";
 
 //These are Third party packages for smooth slideshow
 import { Zoom } from "react-slideshow-image";
-import  "../styles/slider.module.css"
+import styles  from "../styles/slider.module.css"
 import "react-slideshow-image/dist/styles.css";
 
 const Slideshow = () => {
@@ -16,39 +16,32 @@ const Slideshow = () => {
 	];
 
 	//These are custom properties for zoom effect while slide-show
-	function flechas()  {
-		indicators: true;
-		scale: 1.2;
-		duration: 2000;
-		transitionDuration: 500;
-		infinite: true;
+	const flechas = {
+		indicators: true,
+		scale: 1.2,
+		duration: 2000,
+		transitionDuration: 200,
+		infinite: true,
 	}
 	
-	var slider={
-		marginTop:"30px",
-		marginBottom:"30px",
-		
-	};
-
 
 	return (
-		<div className="ave" style={slider}>
-			
+		<div className={styles.slidescontainer}>
 			<center>
 				<Zoom  {...flechas}>
-				{images.map((each, index) => (
-					<div style={{width: "77.79%", display: "flex", justifyContent: "center", alignContent: "center"
-					,border:"2px solid #FFFF",boxShadow:"10px 5px 5px  rgba(0,0,0,0.5)"}} key={index}>
-						<img
-							style={{display: "flex", justifyContent: "center"}}
-							className="w-3/4 object-cover rounded-lg shadow-xl"
-							width={20000}
-							height={600}
-							src={each}
-						/>
-					</div>
-				))}
-			</Zoom>
+					{images.map((each, index) => (
+						<div style={{width: "77.79%", display: "flex", justifyContent: "center", alignContent: "center"
+						,border:"2px solid #FFFF",boxShadow:"10px 5px 5px  rgba(0,0,0,0.5)"}} key={index}>
+							<img
+								style={{display: "flex", justifyContent: "center"}}
+								className="w-3/4 object-cover rounded-lg shadow-xl"
+								width={20000}
+								height={600}
+								src={each}
+							/>
+						</div>
+					))}
+				</Zoom>
 			</center>
 		</div>	
 		
