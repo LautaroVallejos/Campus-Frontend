fcef45e10451f1b00e7d54de327e1d8e34349ff5
franncho <rarugfrancisco@gmail.com>
2022-11-06 21:43:57 -0300

    WIP dropdown, errores en el js

--- components/Homepage.js
+++ components/Homepage.js
@@ -3,87 +3,45 @@ import Image from "next/image";
 
 
 const Homepage = () => {
-    return(
+    /*Error en vincular el java script a SOLUCIONAR*/
+    
+    /*var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }*/
+    return (
         <>
-        
-            <section className={styles.section}>
-                <div className={styles.container}>
-                    <div className={styles.accordion}>
-                        <div className={styles.accordionItem} id="question1">
-                            <a className={styles.accordionLink} href="#question1">
-                            Por que lo hicimos?
-                                <Image className={styles.imgAdd}
-                                    src="/img/max.png"
-                                    alt="logo"
-                                    width={20}
-                                    height={20}
-                                    />
-                                    <Image className={styles.imgMin}
-                                    src="/img/menos.png"
-                                    alt="logo"
-                                    width={20}
-                                    height={20}
-                                    />
-                            </a>
-                            <div className={styles.answer}>
-                                <p>
-                                es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.
-                                </p>
-                            </div>
-                        </div>
-
-                        <div className={styles.accordionItem} id="question2">
-                            <a className={styles.accordionLink} href="#question2">
-                                 Como lo hicimos?
-                                <Image className={styles.imgAdd}
-                                    src="/img/max.png"
-                                    alt="logo"
-                                    width={20}
-                                    height={20}
-                                    />
-                                    <Image className={styles.imgMin}
-                                    src="/img/menos.png"
-                                    alt="logo"
-                                    width={20}
-                                    height={20}
-                                    />
-                            </a>
-                            <div className={styles.answer}>
-                                <p>
-                                es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.
-                                </p>
-                            </div>
-                        </div>
-
-                        <div className={styles.accordionItem} id="question3">
-                            <a className={styles.accordionLink} href="#question3">
-                                Para que sirve?
-                                <Image className={styles.imgAdd}
-                                    src="/img/max.png"
-                                    alt="logo"
-                                    width={20}
-                                    height={20}
-                                    />
-                                    <Image className={styles.imgMin}
-                                    src="/img/menos.png"
-                                    alt="logo"
-                                    width={20}
-                                    height={20}
-                                    />
-                            </a>
-                            <div className={styles.answer}>
-                                 <p>
-                                 es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.
-                                </p>
-                            </div>
-                        </div>
-
-                    </div>
-
-                    
+            <div className={styles.title}>
+                <p className={styles.text}>PARA QUE NOS <em className={styles.em}>Conozcan...</em></p>
+            </div>
+            <div className={styles.container}>
+                <button className={styles.collapsible}>Quienes somos?</button>
+                <div className={styles.content}>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 </div>
-            </section>
+
+                <button className={styles.collapsible}>Que tecnologias usamos para el campus?</button>
+                <div className={styles.content}>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+                <button className={styles.collapsible}>Que funcionalidad tiene este campus?</button>
+                <div className={styles.content}>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+            </div>
+            <br></br>
         </>
+
     )
 }
 export default Homepage;
