92e7b7694dc686ad7232e4f89dd820b24ebe5d31
franncho <rarugfrancisco@gmail.com>
2022-11-08 16:38:24 -0300

    error con los estilos

--- components/Homepage.js
+++ components/Homepage.js
@@ -1,44 +1,44 @@
 import styles from '../styles/Homepage.module.css'
 import Image from "next/image";
+import { Collapse, Text } from "@nextui-org/react";
 
 
 const Homepage = () => {
-    /*Error en vincular el java script a SOLUCIONAR*/
-    
-    /*var coll = document.getElementsByClassName("collapsible");
-    var i;
-
-    for (i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function () {
-            this.classList.toggle("active");
-            var content = this.nextElementSibling;
-            if (content.style.display === "block") {
-                content.style.display = "none";
-            } else {
-                content.style.display = "block";
-            }
-        });
-    }*/
     return (
         <>
             <div className={styles.title}>
                 <p className={styles.text}>PARA QUE NOS <em className={styles.em}>Conozcan...</em></p>
             </div>
-            <div className={styles.container}>
-                <button className={styles.collapsible}>Quienes somos?</button>
-                <div className={styles.content}>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                </div>
 
-                <button className={styles.collapsible}>Que tecnologias usamos para el campus?</button>
-                <div className={styles.content}>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                </div>
-                <button className={styles.collapsible}>Que funcionalidad tiene este campus?</button>
-                <div className={styles.content}>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                </div>
+            <div className={styles.container}>
+                <Collapse.Group>
+                    <Collapse title="Quienes comos?">
+                        <Text>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                            aliquip ex ea commodo consequat.
+                        </Text>
+                    </Collapse>
+                    <Collapse title="Que tecnologias usamos?" expanded>
+                        <Text>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                            aliquip ex ea commodo consequat.
+                        </Text>
+                    </Collapse>
+                    <Collapse  title="Que funcionalidad tiene este campus">
+                        <Text>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                            aliquip ex ea commodo consequat.
+                        </Text>
+                    </Collapse>
+                </Collapse.Group>
             </div>
+            
             <br></br>
         </>
 
